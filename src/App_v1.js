dddd62c90b2e18017f2cd0a05c47ffd1ef2da15f
DIEGO CAMILO MARTINS VIEIRA <dcvieira@tjsp.jus.br>
2024-10-25 16:58:45 -0300

    add fetch movies effect

--- src/App_v1.js
+++ src/App_v1.js
@@ -4,24 +4,15 @@ import Search from "./Search";
 import NumResults from "./NumResults";
 import Box from "./Box";
 import MovieList from "./MovieList";
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import { tempMovieData } from "./data";
+import { tempWatchedData } from "./data";
 import WatchedSummary from "./WatchedSummary";
 import WatchedMoviesList from "./WatchedMoviesList";
 
-const BASE_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=e2300694";
-
 export default function App() {
-  const [movies, setMovies] = useState([]);
-  const [watched, setWatched] = useState([]);
-
-  useEffect(() => {
-    async function fetchMovies() {
-      const response = await fetch(`${BASE_URL}&s=batman`);
-      const data = await response.json();
-      setMovies(data.Search);
-    }
-    fetchMovies();
-  }, []);
+  const [movies, setMovies] = useState(tempMovieData);
+  const [watched, setWatched] = useState(tempWatchedData);
 
   return (
     <>
